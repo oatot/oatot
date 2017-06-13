bc843dfd875fb57e2dcde5e66ef6eb700ed1bf68
Javier Barteaux <javierb@riseup.net>
2017-06-13 15:34:28 +0000

    Switch from C++ to C in protobuf generators

--- genpb.sh
+++ genpb.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+mkdir -p backend/generated
 protoc \
     --go_out=plugins=grpc:backend/generated \
-    --cpp_out=oa/generated \
-    --grpc_out=oa/generated \
-    --plugin=protoc-gen-grpc="$(which grpc_cpp_plugin)" \
+    api.proto
+
+mkdir -p oa/generated
+protoc-c \
+    --c_out=oa/generated \
     api.proto
