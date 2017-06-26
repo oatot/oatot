8fd7ffc28b6c6e804c28b42b79cdb8aa55033bf2
Dio Eraclea <dio@guild-oa.com>
2017-06-26 21:44:23 +0100

    add dummy g_oatot module

---
+++ oa_mod/code/game/g_oatot.c
@@ -0,0 +1,10 @@
+/*
+Interactions with `mod_proxy`.
+*/
+
+#include "g_oatot.h"
+
+int G_oatot_getBalance( const char* cl_guid, const char* currency ) {
+    // TODO dummy, no implementation yet.
+    return 999999;
+}
---
+++ oa_mod/code/game/g_oatot.h
@@ -0,0 +1,10 @@
+/*
+Declarations for interactions with `mod_proxy`.
+*/
+
+#ifndef _G_OATOT_H
+#define _G_OATOT_H
+
+int G_oatot_getBalance( const char* cl_guid, const char* currency );
+
+#endif /* ifndef _G_OATOT_H */
