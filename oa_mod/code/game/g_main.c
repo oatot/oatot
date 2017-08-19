17c41c5c542f5e487df509d9f2a6eeedd08a7c96
Dio Eraclea <dio@guild-oa.com>
2017-08-19 10:07:55 +0100

    Include protobuf headers for test

--- oa_mod/code/game/g_main.c
+++ oa_mod/code/game/g_main.c
@@ -21,6 +21,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 //
 
+#include <protobuf-c/protobuf-c.h>
+
 #include "g_local.h"
 #include "bg_public.h"
 #include "g_oatot.h"
