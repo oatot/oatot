cd905045470c11471a6db0133d80a11c0fb975b0
Dio Eraclea <dio@guild-oa.com>
2017-12-03 03:58:32 +0000

    update genpb.sh

--- genpb.sh
+++ genpb.sh
@@ -5,10 +5,5 @@ protoc \
     --go_out=plugins=grpc:generated \
     api.proto
 
-mkdir -p oa_mod/code/game/generated
-protoc-c \
-    --c_out=oa_mod/code/game/generated \
-    api.proto
-
 sed 's/\<_Oatot_serviceDesc\>/Oatot_serviceDesc/g' \
     -i generated/api.pb.go
