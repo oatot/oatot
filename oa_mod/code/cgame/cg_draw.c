b5d3351c19e9cc42b0199142e829c80444295ffc
Dio Eraclea <dio@guild-oa.com>
2018-01-01 19:48:15 +0000

    Draw info over the lock pic, not the opposite

--- oa_mod/code/cgame/cg_draw.c
+++ oa_mod/code/cgame/cg_draw.c
@@ -3038,13 +3038,13 @@ static void CG_DrawGameStageInfo( void ) {
         CG_DrawBigString(320 - 7 * BIGCHAR_WIDTH, 50, "^2FORMING TEAMS", 1.0F);
         CG_DrawSmallString(320 - 24 * SMALLCHAR_WIDTH, 100, "Type ^2/help ^7to get help, ^2/ready ^7to start betting.", 1.0F);
     } else if ( cgs.gameStage == MAKING_BETS ) {
+        CG_DrawPic(320, 75, 100, 100, cgs.media.lockShader);
         CG_DrawBigString(320 - 6 * BIGCHAR_WIDTH, 50, "^1MAKING BETS", 1.0F);
         CG_DrawSmallString(320 - 12 * SMALLCHAR_WIDTH, 100, "Type ^2/help ^7to get help.", 1.0F);
-        CG_DrawPic(320, 75, 50, 50, cgs.media.lockShader);
     } else if ( cgs.gameStage == PLAYING) {
+        CG_DrawPic(320, 75, 100, 100, cgs.media.lockShader);
         CG_DrawBigString(320 - 4 * BIGCHAR_WIDTH, 50, "^3PLAYING", 1.0F);
         CG_DrawSmallString(320 - 12 * SMALLCHAR_WIDTH, 100, "Type ^2/help ^7to get help.", 1.0F);
-        CG_DrawPic(320, 75, 50, 50, cgs.media.lockShader);
     }
 }
 
