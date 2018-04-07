b20de554a3772e2bd5376c1fc09408220939310b
Dio Eraclea <dio@guild-oa.com>
2018-04-07 19:26:15 +0100

    Adjustments

--- oa_mod/code/cgame/cg_draw.c
+++ oa_mod/code/cgame/cg_draw.c
@@ -2905,16 +2905,16 @@ CG_DrawFlagsStatus
 void CG_DrawFlagsStatus(void) {
     if (cgs.flagsStatus[TEAM_RED].stolen) {
         if (cgs.flagsStatus[TEAM_RED].dropped) {
-            CG_DrawPic(170, 0, 80, 80, cgs.media.droppedFlagShader[TEAM_RED]);
+            CG_DrawPic(150, 0, 40, 40, cgs.media.droppedFlagShader[TEAM_RED]);
         } else {
-            CG_DrawPic(170, 0, 80, 80, cgs.media.flagShader[TEAM_RED]);
+            CG_DrawPic(150, 0, 40, 40, cgs.media.flagShader[TEAM_RED]);
         }
     }
     if (cgs.flagsStatus[TEAM_BLUE].stolen) {
         if (cgs.flagsStatus[TEAM_BLUE].dropped) {
-            CG_DrawPic(390, 0, 80, 80, cgs.media.droppedFlagShader[TEAM_BLUE]);
+            CG_DrawPic(410, 0, 40, 40, cgs.media.droppedFlagShader[TEAM_BLUE]);
         } else {
-            CG_DrawPic(390, 0, 80, 80, cgs.media.flagShader[TEAM_BLUE]);
+            CG_DrawPic(410, 0, 40, 40, cgs.media.flagShader[TEAM_BLUE]);
         }
     }
 }
